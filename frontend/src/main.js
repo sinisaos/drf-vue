f5c965135cac9ccc0cb037d2fcdd7233a4ee03e2
sinisaos <sinisaos@gmail.com>
2024-10-28 16:01:20 +0100

    remove loading plugin

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -5,9 +5,6 @@ import store from "./store"
 import axios from 'axios'
 import App from './App.vue'
 
-import { LoadingPlugin } from 'vue-loading-overlay'
-import 'vue-loading-overlay/dist/css/index.css'
-
 // Add the necessary CSS
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
@@ -15,10 +12,8 @@ import 'bootstrap-vue-next/dist/bootstrap-vue-next.css'
 axios.defaults.withCredentials = true
 axios.defaults.baseURL = "http://localhost:8000"
 
-
 const app = createApp(App)
 app.use(createBootstrap())
-app.use(LoadingPlugin)
 app.use(store)
 app.use(router)
 app.mount('#app')
